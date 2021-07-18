c191514cc28cb7c494126826e3ac966f5a9bceab
Lai-khan <40736969+Lai-khan@users.noreply.github.com>
2021-07-18 15:08:07 +0900

    [issue-1] 회원 인증 기능
    - Passport Guards 적용
      - class-validator 설치 및 Validation Pipe 적용
      - Login route 생성
    - JWT 적용
      - jwt 모듈 설치
      - JWT 생성&리턴 함수 생성
      - Passport Jwt Strategy 구현
      - controller에 jwt 코드 적용

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
 import { AuthService } from './auth.service';
-import { Controller } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { UserService } from './../user/user.service';
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService, private readonly jwtService: JwtService) {}
 
   async validateUser(id: string, password: string): Promise<any> {
     const user = await this.userService.getUserById(id);
@@ -13,5 +14,13 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { userId: user.id };
+    return {
+      user,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
